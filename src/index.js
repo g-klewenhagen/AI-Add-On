318fdad8eda039dd16ee0ed44352cdd335c49963
gabriela klewenhagen <gabriela.klewenhagen@gmail.com>
2024-09-13 21:36:57 +0200

    fixed the error on the apiKey

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generateRecipe(event){
     event.preventDefault();
 
     let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey: 8f4329a8tf75b2b642a61303d375o2c5;
+    let apiKey = 8f4329a8tf75b2b642a61303d375o2c5;
     let context ="You are a creative bartender and love to create new cocktails. You mission is to generate a simple recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Please include a name for the cocktail.";
     let prompt = `User instructions: Generate cocktail recipe using ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
