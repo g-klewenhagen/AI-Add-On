0109d62f07ce9c3f2915c6b4a7ac676edbe8238b
gabriela klewenhagen <gabriela.klewenhagen@gmail.com>
2024-09-13 21:31:14 +0200

    integrated the ShecodesAPI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-
-function generateRecipe(event){
-    event.preventDefault();
-
-    //build API URL
-    //Make a call to the API
-
-    new Typerwriter("#recipe",{
+function displayRecipe(response){
+new Typerwriter("#recipe",{
         strings: "tequila sunrise",
         autostart: true,
         delay: 1,
@@ -13,7 +7,21 @@ function generateRecipe(event){
     });
 }
 
+function generateRecipe(event){
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey: 8f4329a8tf75b2b642a61303d375o2c5;
+    let context ="You are a creative bartender and love to create new cocktails. You mission is to generate a simple recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Please include a name for the cocktail.";
+    let prompt = `User instructions: Generate cocktail recipe using ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let recipeElement = document.querySelector("#recipe");
+    recipeElement.classList.remove("hidden");
+    recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe with ${instructionsInput.value}</div>`;
+
+    axios.get(apiURL).then(displayRecipe);
+}
+
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
-
-//let apiKey: 8f4329a8tf75b2b642a61303d375o2c5
