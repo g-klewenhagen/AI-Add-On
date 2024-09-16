569c24eaa19174f793f7495ced515cf1097db3e6
gabriela klewenhagen <gabriela.klewenhagen@gmail.com>
2024-09-16 20:47:26 +0200

    ux improvements

--- src/index.js
+++ src/index.js
@@ -12,14 +12,13 @@ function generateRecipe(event){
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "8f4329a8tf75b2b642a61303d375o2c5";
-    let context ="You are a creative chef and love to create new recipes fusioning Mexican and German cuisines. You mission is to generate a simple recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Please name each recipe in a hipster fashion.";
+    let context ="You are a creative chef and love to create new recipes fusioning Mexican and German cuisines. You mission is to generate a simple recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Please name each recipe in a hipster fashion. Do not provide an intro.";
     let prompt = `User instructions: Generate a Mexican - German fusion recipe using ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let recipeElement = document.querySelector("#recipe");
     recipeElement.classList.remove("hidden");
     recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe with ${instructionsInput.value}</div>`;
-
     axios.get(apiURL).then(displayRecipe);
 }
 
